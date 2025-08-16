0ef0f0166f873887c42270b0eee58c303d74a882
Nisal Perera <nisal.perera@veracitygp.com>
2025-08-16 09:39:35 +0530

    refactor code

--- launch/gz.launch.py
+++ launch/gz.launch.py
@@ -11,7 +11,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node, SetParameter
-# from launch_ros.logging import get_logger
 
 
 # logging.basicConfig(level=logging.DEBUG)
@@ -32,18 +31,11 @@ def generate_launch_description():
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
-    use_slam = LaunchConfiguration('use_slam')
 
     rviz = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     [os.path.join(get_package_share_directory(package_name),'launch','rviz.launch.py')]), 
                     launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': use_ros2_control}.items(),
-    )
-
-    joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [os.path.join(get_package_share_directory(package_name),'launch','joystick.launch.py')]), 
-                    launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
@@ -63,75 +55,7 @@ def generate_launch_description():
                                    '-entity', package_name],
                         output='screen',
                         parameters=[{'use_sim_time': use_sim_time}]
-                        # namespace=package_name
                     )
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="diff_drive_controller",
-        arguments=["diff_drive_controller"],
-        parameters=[{'use_sim_time': use_sim_time}]
-        # namespace=package_name
-    )
-
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="joint_state_broadcaster",
-        arguments=["joint_state_broadcaster"],
-        parameters=[{'use_sim_time': use_sim_time}]
-        # namespace=package_name
-    )
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','controllers.yaml')
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            controller_params_file,
-            {"use_sim_time": use_sim_time},
-            {"tf_buffer_duration": 10.0}],
-        output="both",
-        respawn=True,
-        # namespace=package_name
-    )
-
-    slam_params_file = os.path.join(get_package_share_directory(package_name),'config','mapper_params_online_async.yaml')
-    slam = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [os.path.join(get_package_share_directory(package_name),'launch','online_async_launch.py')]), 
-                    launch_arguments={'use_sim_time': use_sim_time, 'params_file': slam_params_file}.items(),
-                    condition=IfCondition(use_slam)
-    )
-
-    amcl_params_file = os.path.join(get_package_share_directory(package_name),'config','nav2_params.yaml')
-    map_file = os.path.join(get_package_share_directory(package_name),'maps','map_save.yaml')
-    amcl = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [os.path.join(get_package_share_directory(package_name),'launch','localization.launch.py')]), 
-                    launch_arguments={
-                        'use_sim_time': use_sim_time, 
-                        'params_file': amcl_params_file,
-                        'map': map_file
-                    }.items(),
-                    condition=IfCondition(use_slam)
-    )
-
-    navigation = IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(
-                        [os.path.join(get_package_share_directory(package_name),'launch','navigation.launch.py')]), 
-                        launch_arguments={'use_sim_time': use_sim_time}.items(), 
-                        condition=IfCondition(use_slam)
-    )
-
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel_out','/diff_drive_controller/cmd_vel_unstamped')]
-        )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -148,14 +72,6 @@ def generate_launch_description():
             description='Use NAV2 toolkit if true'),
         OpaqueFunction(function=log_args),
         rviz,
-        joystick,
-        control_node,  # Move this before gazebo
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner,
-        slam,
-        amcl, 
-        navigation,
-        twist_mux,
     ])
