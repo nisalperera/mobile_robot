2b1f753bfec7a9672df5fb3bd2e96a23f152a8d2
Nisal Perera <chinthanapereranisal@gmail.com>
2026-08-29 15:42:30 +0530

    visualize zed2 cams in rviz

---
+++ launch/zed2.launch.py
@@ -0,0 +1,24 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    xacro_file = os.path.join(os.getcwd(), 'mobile_robot', 'description', 'four_zed2_camera_mount.xacro')
+    robot_description = ParameterValue(Command(['xacro ', xacro_file]), value_type=str)
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen'
+        ),
+    ])
